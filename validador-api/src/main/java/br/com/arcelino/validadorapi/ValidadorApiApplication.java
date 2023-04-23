8cc3c317df4c8319c690e078490f1005496622ab
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2023-04-23 11:51:03 -0300

    Import Projeto Base

---
+++ validador-api/src/main/java/br/com/arcelino/validadorapi/ValidadorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.arcelino.validadorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidadorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidadorApiApplication.class, args);
+	}
+
+}
